f7d8311ecac89a99569353963a39693f6029c820
Daniel Platt <github@ofdan.co.uk>
2014-05-02 16:43:10 +0100

    whitespace changes

--- Tests/Utility/BarcodeTest.php
+++ Tests/Utility/BarcodeTest.php
@@ -6,6 +6,7 @@ use Hackzilla\BarcodeBundle\Utility\Barcode;
 
 class BarcodeTest extends \PHPUnit_Framework_TestCase
 {
+
     private $_object;
 
     public function setup()
@@ -15,7 +16,7 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
 
     public function testBarColor()
     {
-        $this->_object->setBarColor(array(123,456,789));
+        $this->_object->setBarColor(array(123, 456, 789));
         $this->assertEquals(123, $this->_object->barColor(0));
         $this->assertEquals(456, $this->_object->barColor(1));
         $this->assertEquals(789, $this->_object->barColor(2));
@@ -28,7 +29,7 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
 
     public function testBgColor()
     {
-        $this->_object->setBgColor(array(123,456,789));
+        $this->_object->setBgColor(array(123, 456, 789));
         $this->assertEquals(123, $this->_object->bgColor(0));
         $this->assertEquals(456, $this->_object->bgColor(1));
         $this->assertEquals(789, $this->_object->bgColor(2));
@@ -41,7 +42,7 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
 
     public function testTextColor()
     {
-        $this->_object->setTextColor(array(123,456,789));
+        $this->_object->setTextColor(array(123, 456, 789));
         $this->assertEquals(123, $this->_object->textColor(0));
         $this->assertEquals(456, $this->_object->textColor(1));
         $this->assertEquals(789, $this->_object->textColor(2));
@@ -123,15 +124,16 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertNotEquals(7, $this->_object->generateEanChecksum('50001594159'));
     }
-    
+
     public function testEncodeEan()
     {
         $bars = $this->_object->encodeEan('978085934063');
         $this->assertEquals('ISBN', $bars['encoding']);
         $this->assertEquals('9a1a1312312111231213132131121a1a1141111323211111414112122a1a', $bars['bars']);
         $this->assertEquals('0:12:9 12:12:7 19:12:8 26:12:0 33:12:8 40:12:5 47:12:9 59:12:3 66:12:4 73:12:0 80:12:6 87:12:3 94:12:2', $bars['text']);
-        
+
         $bars = $this->_object->encodeEan('fail');
         $this->assertEquals('Invalid', \substr($bars['text'], 0, 7));
     }
+
 }
